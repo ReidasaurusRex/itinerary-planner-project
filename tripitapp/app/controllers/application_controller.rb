8f181d537e254a10ffdea1f5709dc3d6d9adbc8f
EDLyonhart <EDLyonhart@gmail.com>
2015-02-28 12:42:32 -0800

    Moved access controller into application controller

--- tripitapp/app/controllers/application_controller.rb
+++ tripitapp/app/controllers/application_controller.rb
@@ -1,5 +1,40 @@
 class ApplicationController < ActionController::Base
+
+before_action :prevent_login_signup, only: [
+  :access_login,
+  :access_signup,
+  :access_create,
+  :access_attempt_login
+]
+before_action :mandatory_login, only: [
+    :itinerary_destinations,
+    :new_itinerary_destination, 
+    :edit_itinerary_destination, 
+    :itinerary_destination, 
+    :itineraries,                 #index
+    :new_itinerary, 
+    :edit_itinerary, 
+    :itinerary,                   #show
+    :users_show, 
+    :users_edit
+]
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+end
+
+
+private
+
+def mandatory_login
+  if session[:user_id].nil?
+    redirect_to access_login_path
+  end
+end
+
+def prevent_login_signup
+  if session[:user_id]
+    redirect_to users_show_path
+  end
 end
