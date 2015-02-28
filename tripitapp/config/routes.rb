9339509fa97d0a60925d8f5e612e98dbd515d681
EDLyonhart <EDLyonhart@gmail.com>
2015-02-28 12:50:02 -0800

    Added routes info

--- tripitapp/config/routes.rb
+++ tripitapp/config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 end
 
 
-#    Prefix Verb   URI Pattern                                                Controller#Action
+# Prefix Verb   URI Pattern                                                Controller#Action
 #     itinerary_destinations GET    /itineraries/:itinerary_id/destinations(.:format)          destinations#index
 #                            POST   /itineraries/:itinerary_id/destinations(.:format)          destinations#create
 #  new_itinerary_destination GET    /itineraries/:itinerary_id/destinations/new(.:format)      destinations#new
@@ -39,10 +39,11 @@ end
 #                            PATCH  /itineraries/:id(.:format)                                 itineraries#update
 #                            PUT    /itineraries/:id(.:format)                                 itineraries#update
 #                            DELETE /itineraries/:id(.:format)                                 itineraries#destroy
-#                 users_show GET    /users/show(.:format)                                      users#show
-#                 users_edit GET    /users/edit(.:format)                                      users#edit
-#               access_login GET    /access/login(.:format)                                    access#login
-#              access_signup GET    /access/signup(.:format)                                   access#signup
-#              access_create GET    /access/create(.:format)                                   access#create
-#       access_attempt_login GET    /access/attempt_login(.:format)                            access#attempt_login
-
+#                       root GET    /                                                          access#login
+#                       user GET    /users/show(.:format)                                      users#show
+#                  user_edit GET    /users/edit(.:format)                                      users#edit
+#                 users_edit PUT    /users/edit(.:format)                                      users#edit
+#                      login GET    /access/login(.:format)                                    access#login
+#                     signup GET    /access/signup(.:format)                                   access#signup
+#                create_user POST   /access/create(.:format)                                   access#create
+#       access_attempt_login POST   /access/attempt_login(.:format)                            access#attempt_login
