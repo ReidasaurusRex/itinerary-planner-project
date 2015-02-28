56751211924b5cb9dd7ab7076e431cddf73bdef1
ReidasaurusRex <reidhlewis91@gmail.com>
2015-02-28 12:44:14 -0800

    Added some functionality to destinations controller.

--- tripitapp/app/controllers/destinations_controller.rb
+++ tripitapp/app/controllers/destinations_controller.rb
@@ -1,20 +1,34 @@
 class DestinationsController < ApplicationController
-  def new
+  before_action :find_itin
 
+  def new
   end
 
   def create
-
+    @itinerary.destinations.create destination_params 
   end
 
   def show
-  @itinerary = Itinerary.find(params[:itinerary_id])
-  @destination = @itinerary.destinations.where(:id == params[:id])
+    find_destination @itinerary
   end
 
   def edit
+    find_destination @itinerary
   end
 
   def delete
+  end
+
+  private
+  def destination_params
+    params.require(:destination).permit(:id, :location, :name, :date)
+  end
+
+  def find_itin
+    @itinerary = Itinerary.find(params[:itinerary_id])
+  end
+
+  def find_destination itinerary
+    @destination = itinerary.destinations.find(params[:id])
   end
 end
