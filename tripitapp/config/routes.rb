ca8fceb47920afcec5f38bb71591f648865ae974
EDLyonhart <EDLyonhart@gmail.com>
2015-02-28 15:54:04 -0800

    fixed routes

--- tripitapp/config/routes.rb
+++ tripitapp/config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   root 'access#login'
 
-  get 'users/:id', as: :user_show
+  get 'users/:id', to: "users#show", as: :user_show
 
-  get 'user/:id/edit', as: :user_edit
+  get 'users/:id/edit', to: "users#user_edit", as: :user_edit
 
-  put 'user/:id/edit'
+  put 'users/:id/edit', to: "users#update"
 
   get 'access/login', as: :login
 
@@ -22,3 +22,31 @@ Rails.application.routes.draw do
   delete 'logout', to: "access#logout", as: :logout
 
 end
+
+
+#     Prefix Verb   URI Pattern                                                Controller#Action
+#     itinerary_destinations GET    /itineraries/:itinerary_id/destinations(.:format)          destinations#index
+#                            POST   /itineraries/:itinerary_id/destinations(.:format)          destinations#create
+#  new_itinerary_destination GET    /itineraries/:itinerary_id/destinations/new(.:format)      destinations#new
+# edit_itinerary_destination GET    /itineraries/:itinerary_id/destinations/:id/edit(.:format) destinations#edit
+#      itinerary_destination GET    /itineraries/:itinerary_id/destinations/:id(.:format)      destinations#show
+#                            PATCH  /itineraries/:itinerary_id/destinations/:id(.:format)      destinations#update
+#                            PUT    /itineraries/:itinerary_id/destinations/:id(.:format)      destinations#update
+#                            DELETE /itineraries/:itinerary_id/destinations/:id(.:format)      destinations#destroy
+#                itineraries GET    /itineraries(.:format)                                     itineraries#index
+#                            POST   /itineraries(.:format)                                     itineraries#create
+#              new_itinerary GET    /itineraries/new(.:format)                                 itineraries#new
+#             edit_itinerary GET    /itineraries/:id/edit(.:format)                            itineraries#edit
+#                  itinerary GET    /itineraries/:id(.:format)                                 itineraries#show
+#                            PATCH  /itineraries/:id(.:format)                                 itineraries#update
+#                            PUT    /itineraries/:id(.:format)                                 itineraries#update
+#                            DELETE /itineraries/:id(.:format)                                 itineraries#destroy
+#                       root GET    /                                                          access#login
+#                  user_show GET    /users/:id(.:format)                                       users#show
+#                  user_edit GET    /users/:id/edit(.:format)                                  users#user_edit
+#                            PUT    /users/:id/edit(.:format)                                  users#update
+#                      login GET    /access/login(.:format)                                    access#login
+#                     signup GET    /access/signup(.:format)                                   access#signup
+#                create_user POST   /access/create(.:format)                                   access#create
+#       access_attempt_login POST   /access/attempt_login(.:format)                            access#attempt_login
+#                     logout DELETE /logout(.:format)                                          access#logout
