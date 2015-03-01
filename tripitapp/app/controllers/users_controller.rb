268fbd552da31b99aee09cf243bd7fc5784ca326
EDLyonhart <EDLyonhart@gmail.com>
2015-02-28 21:15:31 -0800

    User show page

--- tripitapp/app/controllers/users_controller.rb
+++ tripitapp/app/controllers/users_controller.rb
@@ -1,27 +1,30 @@
 class UsersController < ApplicationController
 
-before_action :find_user, only: [:show, :user_edit, :update]
-before_action :user_params, only: [:user_edit, :update]
+before_action :find_user, only: [:show, :user_edit]
+before_action :user_params, only: [:user_edit]
 
   def index          
     @users = Users.all
   end
 
+  def user_edit     #get 'users/:id/edit', to: "users#user_edit", as: :user_edit
+  end
+
   def show            #get 'users/:id', to: "users#show", as: :user_show
+    #@itineraries = Itineraries.all ... but only this users
+    #@destinations = Destinations.all ... that this user is going on
   end
 
   def update          #put 'users/:id/edit', to: "users#update"
-    @user.update_attributes user_params
+    @user.update_attributes
     redirect_to @user
   end
 
-  def user_edit     #get 'users/:id/edit', to: "users#user_edit", as: :user_edit
-  end
 
 
   private
   def user_params
-    params.require(:user).permit(:username, :password, :password_digest)
+    params.require(:user).permit(:first_name, :last_name, :username, :email) #???"param is missing or the value is empty". neither is true
   end
 
   def find_user
