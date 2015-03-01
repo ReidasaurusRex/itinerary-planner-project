7eccbafa3b0c6e721e7b58643eac0cbdbfa173b4
EDLyonhart <EDLyonhart@gmail.com>
2015-03-01 14:08:54 -0800

    Create User working

--- tripitapp/app/controllers/access_controller.rb
+++ tripitapp/app/controllers/access_controller.rb
@@ -1,56 +1,26 @@
 class AccessController < ApplicationController
 
-  before_action :mandatory_login, only: [
-    :itinerary_destinations,
-    :new_itinerary_destination, 
-    :edit_itinerary_destination, 
-    :itinerary_destination, 
-    :itineraries,                 #index
-    :new_itinerary, 
-    :edit_itinerary, 
-    :itinerary,                   #show
-    :users_show, 
-    :users_edit
-  ]
-  # before_action :user_params, only: [
-  #   :create_user, 
-  #   :login, 
-  #   :access_attempt_login
-  # ]
-  
-  before_action :prevent_login_signup, only: [:signup, :login]
-
-  # before_action :user_params, only: [
-  #   :create_user, 
-  #   :login, 
-  #   :access_attempt_login
-  # ]
-
-
   def signup            #get 'access/signup', as: :signup
     @user = User.new
   end
 
   def login               #/access/login GET
-
   end
 
   def create_user         #/access/create POST
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_digest)
+
     @user = User.create user_params
     if @user.save
       session[:user_id] = @user.id
       redirect_to login_path, notice: "User created successfully, please log in."   #redirect logged in && to home/index-y page
     else
-      redirect_to access_signup_path, notice: "Could not create user, please try again."
+      redirect_to signup_path, notice: "Could not create user, please try again."
     end
   end
 
-  def access_attempt_login
-
-  def login               #get 'access/login', as: :login
-  end
-
   def access_attempt_login   #post 'access/attempt_login'
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_digest)
 
     if params[:user_name].present? && params[:password].present?
       found_user = User.where(username: params[:username]).first
@@ -58,20 +28,19 @@ class AccessController < ApplicationController
         authorized_user = found_user.authenticate(params[:password])
         if authorized_user
           session[:user_id] = authorized_user.id
-          redirect_to user_show_path, notice: "You are successfully logged in" #I think we need a users path which displays their itineraries.
+          redirect_to user_path, notice: "You are successfully logged in" #I think we need a users path which displays their itineraries.
         else
-          redirect_to access_login_path, notice: "Invalid username/password"
+          redirect_to login_path, notice: "Invalid username/password"
         end
       else
-        redirect_to access_login_path, notice: "Invalid username/password"
+        redirect_to login_path, notice: "Invalid username/password"
       end
     else
-      redirect_to access_login_path, notice: "Please enter username/password"
+      redirect_to login_path, notice: "Please enter username/password"
     end
   end
 
   def create_user         #post 'access/create', as: :signup_user
-    binding.pry
     @user = User.create user_params
     if @user.save
       session[:user_id] = @user.id
@@ -83,8 +52,7 @@ class AccessController < ApplicationController
 
   def logout                 #delete 'logout', to: "access#logout", as: :logout
     session[:user_id] = nil
-    redirect_to access_login_path, notice: "Thanks for logging out. Check back later"
-  end
+    redirect_to login_path, notice: "Thanks for logging out. Check back later"
   end
 
 end
@@ -92,20 +60,5 @@ end
 private
 
 def user_params
-  params.require(:user).permit(:username, :password, :password_digest)
-
   params.require(:user).permit(:first_name, :last_name, :username, :email, :password, :password_digest)
-
-end
-
-def mandatory_login
-  if session[:user_id].nil?
-    redirect_to access_login_path
-  end
-end
-
-def prevent_login_singup
-  if session[] != nil
-    redirect_to user_show_path
-  end
 end
--- tripitapp/app/controllers/application_controller.rb
+++ tripitapp/app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
 
-before_action :prevent_login_signup, only: [
-  :access_login,
-  :access_signup,
-  :access_create,
-  :access_attempt_login
-]
+
 before_action :mandatory_login, only: [
     :itinerary_destinations,
     :new_itinerary_destination, 
@@ -17,10 +12,7 @@ before_action :mandatory_login, only: [
     :itinerary,                   #show
     :users_show, 
     :users_edit
-]
-
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+] 
   protect_from_forgery with: :exception
 end
 
@@ -29,12 +21,6 @@ private
 
 def mandatory_login
   if session[:user_id].nil?
-    redirect_to access_login_path
-  end
-end
-
-def prevent_login_signup
-  if session[:user_id]
-    redirect_to users_show_path
+    redirect_to login_path
   end
 end
