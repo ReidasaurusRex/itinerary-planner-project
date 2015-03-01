925a04ce700a674b37ad1fc049d394bb09bbe919
EDLyonhart <EDLyonhart@gmail.com>
2015-03-01 13:28:41 -0800

    A little work in users

--- tripitapp/app/controllers/users_controller.rb
+++ tripitapp/app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-before_action :find_user, only: [:show, :user_edit]
 before_action :user_params, only: [:user_edit]
 
   def index          
@@ -8,9 +7,12 @@ before_action :user_params, only: [:user_edit]
   end
 
   def user_edit     #get 'users/:id/edit', to: "users#user_edit", as: :user_edit
+    @user = User.find(params[:id])
+    params.require(@user).permit(:first_name, :last_name, :username, :email)
   end
 
   def show            #get 'users/:id', to: "users#show", as: :user_show
+    @user = User.find(params[:id])
     #@itineraries = Itineraries.all ... but only this users
     #@destinations = Destinations.all ... that this user is going on
   end
@@ -23,12 +25,5 @@ before_action :user_params, only: [:user_edit]
 
 
   private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :email) #???"param is missing or the value is empty". neither is true
-  end
-
-  def find_user
-    @user = User.find(params[:id])
-  end
-
+ 
 end
